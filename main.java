bf7b0c61438a177348ff269cb58790d117da2801
Dalenda87 <djambiad@3il.fr>
2025-09-28 17:48:22 +0200

    j'ai ajouter une fonction pour la parité

--- main.java
+++ main.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 class Main{
+    public static boolean pair(int number){
+        return number%2 == 0;
+    }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Bonjour je suis un message");
+        System.out.println("Votre nombre :");
+        int number = sc.nextInt();
+
+        if(pair(number)) System.out.println("je suis pair");
+        else System.out.println("Je ne suis pas pair");
     }
 }
